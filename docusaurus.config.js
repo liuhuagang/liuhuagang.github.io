e502cc6b7156dd4e19da800243b6830ae9492e19
小刚 <709777172@qq.com>
2025-05-07 09:03:50 +0800

    Fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'liuhuagang', // Usually your GitHub org/user name.
   projectName: 'liuhuagang.github.io', // Usually your repo name.
-
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
